ddbe2e805f18aba27350fced60fcee3c7c7ef982
uRyanxD <102924776+uRyanxD@users.noreply.github.com>
2022-05-10 20:33:29 -0300

    Upstream (Paper)

--- scripts/basepaperclip.sh
+++ scripts/basepaperclip.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-cd ./Paperclip
-git fetch origin master
-git reset --hard 2d4c7b3
-cd ..
 cp ../../PandaSpigot-Server/build/libs/pandaspigot-1.8.8-R0.1-SNAPSHOT.jar ./Paperclip/pandaspigot-1.8.8.jar
 cp ./work/1.8.8/1.8.8.jar ./Paperclip/minecraft_server.1.8.8.jar
 cd ./Paperclip
 sed -i -e 's/http:\/\/clojars/https:\/\/clojars/g' ./java8/pom.xml # Replace http repository with https
-mvn --batch-mode -Dmcver=1.8.8 -Dvanillajar=../minecraft_server.1.8.8.jar -Dpaperjar=../pandaspigot-1.8.8.jar clean package
+mvn clean package --batch-mode -Dmcver=1.8.8 "-Dpaperjar=../pandaspigot-1.8.8.jar" "-Dvanillajar=../minecraft_server.1.8.8.jar"
 cd ..
-cp ./Paperclip/assembly/target/paperclip-1.8.8.jar ../../PandaSpigot-Server/build/libs/pandaspigot.jar
+cp ./Paperclip/assembly/target/paperclip*.jar ../../PandaSpigot-Server/build/libs/pandaspigot.jar
 
 echo ""
 echo ""
