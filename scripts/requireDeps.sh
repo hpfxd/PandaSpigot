066058e6b09e0afffbd3777cf00f1374f15e5d70
uRyanxD <102924776+uRyanxD@users.noreply.github.com>
2024-01-28 15:04:56 -0300

    [skip ci] Update link

--- scripts/requireDeps.sh
+++ scripts/requireDeps.sh
@@ -13,8 +13,8 @@ if [ -z "${1:-}" ]; then
     _is_dep_available patch
     _is_dep_available curl
 
-    _is_dep_available javac "was not found; you can download the JDK from https://adoptopenjdk.net/ or via your package manager"
-    _is_dep_available jar "was not found; you can download the JDK from https://adoptopenjdk.net/ or via your package manager"
+    _is_dep_available javac "was not found; you can download the JDK from https://adoptium.net/ or via your package manager"
+    _is_dep_available jar "was not found; you can download the JDK from https://adoptium.net/ or via your package manager"
 else
     # Require all dependencies provided.
     for dep in $@; do
