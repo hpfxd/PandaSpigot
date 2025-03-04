264211889003fc67365bdeec9cb7423d4da330b3
uRyanxD <102924776+uRyanxD@users.noreply.github.com>
2025-03-04 20:53:31 -0300

    Avoid overwriting the file manually (#298)

--- scripts/importmcdev.sh
+++ scripts/importmcdev.sh
@@ -9,10 +9,18 @@ source "$basedir/scripts/functions.sh"
 gitcmd="git -c commit.gpgsign=false"
 
 workdir="$basedir/base"
-minecraftversion=$(cat "$workdir/Paper/BuildData/info.json"  | grep minecraftVersion | cut -d '"' -f 4)
+minecraftversion=$(cat "$workdir/Paper/BuildData/info.json" | grep minecraftVersion | cut -d '"' -f 4)
 decompiledir="$workdir/mc-dev/spigot"
 
-find "$decompiledir/$nms" -name '*.java' -type f -exec cp -nt "$workdir/Paper/PaperSpigot-Server/src/main/java/$nms" {} +
+find "$decompiledir/$nms" -type f -name "*.java" | while read file; do
+    filename=$(basename "$file")
+    target="$workdir/Paper/PaperSpigot-Server/src/main/java/$nms/$filename"
+
+    if [[ ! -f "$target" ]]; then
+        cp "$file" "$target"
+    fi
+done
+
 cp -rt "$workdir/Paper/PaperSpigot-Server/src/main/resources" "$decompiledir/assets" "$decompiledir/yggdrasil_session_pubkey.der"
 
 (
@@ -22,16 +30,6 @@ cp -rt "$workdir/Paper/PaperSpigot-Server/src/main/resources" "$decompiledir/ass
         $gitcmd reset --hard HEAD^
     fi
 )
-
-########################################################
-########################################################
-########################################################
-#                   NMS IMPORTS
-# Temporarily add new NMS dev imports here before you run paper patch
-# but after you have paper rb'd your changes, remove the line from this file before committing.
-# we do not need any lines added to this file for NMS
-
-# import FileName
 
 ########################################################
 ########################################################
