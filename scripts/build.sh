6cb54152d1b1bbef345a26b73588e1357af84302
uRyanxD <familiarodrigues123ro@gmail.com>
2025-03-04 16:45:09 -0300

    Script improvements

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,9 +13,9 @@ if [ "$2" == "--setup" ] || [ "$2" == "--jar" ]; then
     ./scripts/decompile.sh "$basedir"
     ./scripts/init.sh "$basedir"
 fi
-./scripts/applyPatches.sh "$basedir" "$2"
 
 if [ "$2" == "--jar" ]; then
+    ./scripts/applyPatches.sh "$basedir" "$2"
     ./gradlew build && ./scripts/paperclip.sh "$basedir"
 fi
 ) || exit 1
