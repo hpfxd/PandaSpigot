8a3fa422546e7f566d80aec22fcf69173e37316e
uRyanxD <familiarodrigues123ro@gmail.com>
2025-04-26 15:34:19 -0300

    revert 2

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,14 +5,10 @@ set -e
 basedir="$(cd "$1" && pwd -P)"
 gitcmd="git -c commit.gpgsign=false"
 
-if [ "$2" == "--jar" ] && [ "$3" == "--fast" ]; then
-    echo "Skipping PandaSpigot setup because --jar and --fast are specified."
-else
-    ($gitcmd submodule update --init --recursive && ./scripts/remap.sh "$basedir" && ./scripts/decompile.sh "$basedir" && ./scripts/init.sh "$basedir" && ./scripts/applyPatches.sh "$basedir" "$2") || (
-        echo "PandaSpigot setup failed"
-        exit 1
-    ) || exit 1
-fi
+($gitcmd submodule update --init --recursive && ./scripts/remap.sh "$basedir" && ./scripts/decompile.sh "$basedir" && ./scripts/init.sh "$basedir" && ./scripts/applyPatches.sh "$basedir" "$2") || (
+    echo "PandaSpigot setup failed"
+    exit 1
+) || exit 1
 
 if [ "$2" == "--jar" ]; then
     ./gradlew build && ./scripts/paperclip.sh "$basedir"
