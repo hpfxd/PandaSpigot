80909a9cf079a9472ff304367d6f5d3eb1e8c82b
uRyanxD <familiarodrigues123ro@gmail.com>
2025-04-26 14:59:17 -0300

    Improve

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,15 +5,10 @@ set -e
 basedir="$(cd "$1" && pwd -P)"
 gitcmd="git -c commit.gpgsign=false"
 
-$gitcmd submodule update --init --recursive
-cd "$basedir"
-
-if [ "$2" == "--setup" ] || [ "$2" == "--jar" ]; then
-    ./scripts/remap.sh "$basedir"
-    ./scripts/decompile.sh "$basedir"
-    ./scripts/init.sh "$basedir"
-fi
-./scripts/applyPatches.sh "$basedir" "$2"
+($gitcmd submodule update --init --recursive && ./scripts/remap.sh "$basedir" && ./scripts/decompile.sh "$basedir" && ./scripts/init.sh "$basedir" && ./scripts/applyPatches.sh "$basedir" "$2") || (
+    echo "Failed to start PandaSpigot"
+    exit 1
+) || exit 1
 
 if [ "$2" == "--jar" ]; then
     ./gradlew build && ./scripts/paperclip.sh "$basedir"
