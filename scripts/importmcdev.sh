a238ecf4e056b3c9da834e568c6d0e28b5852ba6
uRyanxD <familiarodrigues123ro@gmail.com>
2025-04-27 16:21:27 -0300

    revert more

--- scripts/importmcdev.sh
+++ scripts/importmcdev.sh
@@ -12,11 +12,11 @@ workdir="$basedir/base"
 minecraftversion=$(cat "$workdir/Paper/BuildData/info.json" | grep minecraftVersion | cut -d '"' -f 4)
 decompiledir="$workdir/mc-dev/spigot"
 
-find "$decompiledir/$nms" -type f -name "*.java" | while read -r file; do
+find "$decompiledir/$nms" -type f -name "*.java" | while read file; do
     filename="$(basename "$file")"
     target="$workdir/Paper/PaperSpigot-Server/src/main/java/$nms/$filename"
 
-    if [ ! -f "$target" ]; then
+    if [[ ! -f "$target" ]]; then
         cp "$file" "$target"
     fi
 done
