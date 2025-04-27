21b35e9ec78294c0c5028b9d2ffea3b9015d1982
uRyanxD <familiarodrigues123ro@gmail.com>
2025-04-26 23:40:08 -0300

    We have not repackaged nms

--- scripts/init.sh
+++ scripts/init.sh
@@ -33,14 +33,14 @@ echo "Applying CraftBukkit patches to NMS..."
 cd "$workdir/Paper/CraftBukkit"
 $gitcmd checkout -B patched HEAD >/dev/null 2>&1
 rm -rf "$cb"
+mkdir -p "$cb"
 # create baseline NMS import so we can see diff of what CB changed
 while IFS= read -r -d '' file
 do
     patchFile="$file"
-    file="$(echo "$file" | cut -d "/" -f2- | cut -d. -f1).java"
+    file="$(echo "$file" | cut -d. -f1).java"
     mkdir -p "$(dirname $cb/"$file")"
-    cp "$nms/$file" "$cb/$file"
-done <   <(find nms-patches -type f -print0)
+done < <(find nms-patches -type f -print0)
 $gitcmd add --force src
 $gitcmd commit -q -m "Minecraft $ $(date)" --author="Vanilla <auto@mated.null>"
 
@@ -48,16 +48,15 @@ $gitcmd commit -q -m "Minecraft $ $(date)" --author="Vanilla <auto@mated.null>"
 while IFS= read -r -d '' file
 do
     patchFile="$file"
-    file="$(echo "$file" | cut -d "/" -f2- | cut -d. -f1).java"
+    file="$(echo "$file" | cut -d. -f1).java"
 
     echo "Patching $file < $patchFile"
     set +e
     strip_cr "$nms/$file" > /dev/null
     set -e
 
-    "$patch" -s -d src/main/java -p 1 < "$patchFile"
-done <   <(find nms-patches -type f -print0)
-
+    "$patch" -s -d src/main/java/ "net/minecraft/server/$file" < "$patchFile"
+done < <(find nms-patches -type f -print0)
 
 $gitcmd add --force src
 $gitcmd commit -q -m "CraftBukkit $ $(date)" --author="CraftBukkit <auto@mated.null>"
